67a6c3327cf02dc71f68da60697f8e1ffb60f1ab
harveyliu <harvey6v@harveyliu-virtual-machine.localdomain>
2023-02-11 10:44:53 +0800

    Fraction sample code

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+using namespace std;
+
+class Fraction {
+public:
+    Fraction(int num, int den = 1) : m_numerator(num), m_denominator(den) {
+        cout << "Fraction Constructor is called!" << endl;
+    }
+
+    operator double() const {
+        cout << "Fraction double() is called!" << endl;
+
+        return (double)(m_numerator / m_denominator);
+    }
+
+    friend double operator+(int i, Fraction f) {
+        cout << "Fraction operator+() is called!" << endl;
+
+        return (double)i + (double)f;
+    }
+
+private:
+    int m_numerator; // Fen Zi
+    int m_denominator; // Fen Mu
+};
+
+int main(int argc, char** argv) {
+
+    Fraction f(3, 5);
+    double d = 4 + f;
+
+    return 0;
+}
